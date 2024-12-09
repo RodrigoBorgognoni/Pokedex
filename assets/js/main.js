00f1ca2217fb6750ddffca7317c99fbe2d223b60
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2024-12-09 15:14:16 -0300

    create and add dynamically pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,24 @@
 const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const pokemonList = document.getElementById('pokemon_list');
+
+//Function to capitalize only the first letter
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+    /*
+    string = 'pikachu'
+    string.charAt(0) = p
+    string.slice(1) = ikachu
+    */
+}
 
 //function to create an html structure of the pokemon
 function pokemonToLi(pokemon) {
     return `
             <li class="pokemon">
                 <span class="number">#001</span>
-                <span class="name">${pokemon.name}</span>
+                <span class="name">${capitalizeFirstLetter(pokemon.name)}</span>
                 <div class="detail">
                     <ol class="types">
                         <li class="type">Grass</li>
@@ -17,6 +28,13 @@ function pokemonToLi(pokemon) {
                 </div>
             </li>
             `;
+}
+
+//function to create dynamically and add a new pokemon to our list
+function createPokemon(pokemon) {
+    const newPokemon = document.createElement('li'); //creates a new li
+    newPokemon.innerHTML = pokemonToLi(pokemon); //calls our pokemonToLi to create the whole structure of our pokemon with .innerhtml
+    pokemonList.appendChild(newPokemon); //adds the new li as last child to pokemonList
 }
 
 //Async function to call PokeApi
@@ -34,21 +52,22 @@ async function fetchData(url) {
 
 // Function to convert data into HTML
 function convertDataToHtml(data) {
-    // Returns only the pokemon (array of results) of data object
-    const pokemonList = data.results;
-    debugger;
+    const pokemons = data.results; // Returns only the pokemon (array of results) of data object
     // Loop 'for' to iterate Pokemon list
-    for (let index = 0; index < pokemonList.length; index++) {
-        const pokemon = pokemonList[index];
-        console.log(pokemonToLi(pokemon));
+    for (let index = 0; index < pokemons.length; index++) {
+        const pokemon = pokemons[index];
+        console.log(pokemon);
+        createPokemon(pokemon); //calls our function and adds pokemon based on its index
     }
 }
 
-async function main(url) {
+async function loadPokemon(url) {
     const data = await fetchData(url);
     if (data) {
         convertDataToHtml(data);
-    } else { window.alert('Unable to Load Pokemons') }
+    } else {
+        window.alert('Unable to Load Pokemons');
+    }
 }
 
-main(url);
+loadPokemon(url);
