9f7369316157b7af155d42d1369c56eb3a091a34
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2024-12-09 10:10:08 -0300

    created async function to fetch data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,25 @@
-
 const offset = 0;
 const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?${offset}&${limit}`;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function (response){
-    console.log(response)
-})
+/* fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Erro ao buscar dados:', error))
+    .finally(() => console.log('Requisição Concluída')); */
 
+//Async function to call PokeApi
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+    } finally {
+        console.log('Requisição Concluída');
+    }
+}
+
+//Calling async function fetchData with desired url as param
+fetchData(url);
