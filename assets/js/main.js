11fbcd64a9881432c4215ecc2c1bf43aa3d18763
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2024-12-09 11:40:29 -0300

    separated fetch logic and pokemon iteration  into diferent functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,29 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-/* fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error('Erro ao buscar dados:', error))
-    .finally(() => console.log('Requisição Concluída')); */
+//function to create an html structure of the pokemon
+function pokemonToLi(pokemon) {
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">Grass</li>
+                        <li class="type">Poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}" />
+                </div>
+            </li>
+            `;
+}
 
 //Async function to call PokeApi
 async function fetchData(url) {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        return data;
     } catch (error) {
         console.error('Erro ao buscar dados:', error);
     } finally {
@@ -21,5 +32,23 @@ async function fetchData(url) {
     }
 }
 
-//Calling async function fetchData with desired url as param
-fetchData(url);
+// Function to convert data into HTML
+function convertDataToHtml(data) {
+    // Returns only the pokemon (array of results) of data object
+    const pokemonList = data.results;
+    debugger;
+    // Loop 'for' to iterate Pokemon list
+    for (let index = 0; index < pokemonList.length; index++) {
+        const pokemon = pokemonList[index];
+        console.log(pokemonToLi(pokemon));
+    }
+}
+
+async function main(url) {
+    const data = await fetchData(url);
+    if (data) {
+        convertDataToHtml(data);
+    } else { window.alert('Unable to Load Pokemons') }
+}
+
+main(url);
